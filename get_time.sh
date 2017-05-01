f41e1b074f5cb83be9de38e72d037147ba9dce8b
EtraStyle <sebastian.aedo@sansano.usm.cl>
2017-04-30 22:54:53 -0300

    fixed hours and minutes

--- get_time.sh
+++ get_time.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ps aux|grep "python3.*SansanoBot"|sed -e "s/:/ /g"|head -n 1|awk '{print "Sansano bot total runtime: " int($10/24) " days and " $10%24+$11 " hours" }'
+ps aux|grep "python3.*SansanoBot"|sed -e "s/:/ /g"|head -n 1|awk '{print "Sansano bot total runtime: " int($10/24) " days, " $10%24 " hours and " $11 " minutes." }'
