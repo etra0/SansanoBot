1d88d63a17777de11afe7e3dc67397bbf04af92e
EtraStyle <sebastian.aedo@sansano.usm.cl>
2017-05-10 13:44:41 -0300

    script now is more readable

--- get_time.sh
+++ get_time.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-ps aux|grep "python3.*SansanoBot"|sed -e "s/:/ /g"|head -n 1|awk '{print "Sansano bot total runtime: " int($10/24) " days, " $10%24 " hours and " $11 " minutes." }'
+ps aux \
+	| grep "python3.*SansanoBot" \
+	| sed -e "s/:/ /g" \
+	| head -n 1 \
+	| awk '{print "Sansano bot total runtime: " int($10/24) " days, " $10%24 " hours and " $11 " minutes." }'
